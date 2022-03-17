cb8a30546739b90a54b387de658c8a1e5439ce86
ysdwn1107 <84497708+ysdwn1107@users.noreply.github.com>
2022-03-18 01:11:52 +0800

    Homework4
    1

---
+++ Homework4/Homework4_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_1
+{
+	class Program
+	{
+		//链表节点
+		public class Node<T>
+		{
+			public Node<T> Next { get; set; }
+			public T Data { get; set; }
+
+			public Node(T t)
+			{
+				Next = null;
+				Data = t;
+			}
+		}
+
+		//泛型链表类
+		public class GenericList<T>
+		{
+			private Node<T> head;
+			private Node<T> tail;
+
+			public GenericList()
+			{
+				tail = head = null;
+			}
+
+			public Node<T> Head
+			{
+				get => head;
+			}
+
+			public void Add(T t)
+			{
+				Node<T> n = new Node<T>(t);
+				if (tail == null)
+				{
+					head = tail = n;
+				}
+				else
+				{
+					tail.Next = n;
+					tail = n;
+				}
+			}
+
+			public void ForEach(Action<T> action)
+			{
+				for(Node<T> n = head; n != null;n = n.Next)
+				{
+					action(n.Data);
+				}
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+			GenericList<int> intList = new GenericList<int>();
+			for (int x = 0; x < 10; x++)
+			{
+				intList.Add(x);
+			}
+			//打印链表元素
+			intList.ForEach(n => Console.Write(n+" "));
+			//求最大值，最小值，求和
+			int min = intList.Head.Data, max = intList.Head.Data,sum = 0;
+			intList.ForEach(n =>
+			{
+				min = (n < min) ? n : min;
+				max = (n > max) ? n : max;
+				sum += n;
+			});
+			Console.WriteLine("min: " + min + " max: " + max + " sum: " + sum);
+		}
+	}
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Homework4_2
+{
+	class Clock
+	{
+		public delegate void TickEventHandler(Clock sender,DateTime date);
+		public event TickEventHandler TickEvent;
+
+		public delegate void AlarmEventHandler(Clock sender);
+		public event AlarmEventHandler AlarmEvent;
+
+		public Clock(DateTime alarmTime)
+		{
+			AlarmTime = alarmTime;
+		}
+		public DateTime AlarmTime { get; set; }
+
+		public void Run()
+		{
+			while (true)
+			{
+				TickEvent(this,DateTime.Now);
+				if (DateTime.Compare(DateTime.Now, AlarmTime) >= 0)
+				{
+					AlarmEvent(this);
+				}
+				Thread.Sleep(1000);
+			}
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter the number of seconds after the alarm will ring: ");
+			DateTime d1 = DateTime.Now.AddSeconds(Convert.ToDouble(Console.ReadLine()));
+			Clock clock1 = new Clock(d1);
+			clock1.TickEvent += ShowTime;
+			clock1.AlarmEvent += Alarm;
+			new Thread(clock1.Run).Start();
+		}
+
+		public static void ShowTime(Clock sender,DateTime dateTime)
+		{
+			Console.WriteLine("Tick Event: " + dateTime.ToLongTimeString().ToString());
+		}
+
+		public static void Alarm(Clock sender)
+		{
+			Console.WriteLine("Alarm Event: " +DateTime.Now.ToLongTimeString().ToString());
+		}
+	}
+}
+
